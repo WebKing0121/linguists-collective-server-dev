d95debe9d14396379468927efeb4e10f33ba9d56
webking0121 <bestwebdev010@gmail.com>
2020-09-16 11:09:33 +0100

    implemented file upload method

--- src/graphql/translation-assignment/translation-assignment.mutation.ts
+++ src/graphql/translation-assignment/translation-assignment.mutation.ts
@@ -1,77 +1,86 @@
 import { resolver as rs } from 'graphql-sequelize';
-import { TranslationAssignment, TranslationAssignmentFile } from '../../models';
+import { createWriteStream, unlink } from 'fs';
+import * as shortid from 'shortid';
+import {
+    TranslationAssignment, TranslationAssignmentFile
+} from '../../models';
 import to from 'await-to-js';
-const multer = require('multer');
+const Promise = require('bluebird');
+const mkdirp = require('mkdirp');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-      cb(null, 'assets/uploads');
-  },
-  filename: function (req, file, cb) {
-      // You could rename the file name
-      // cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-      console.log('file', file);
-      // You could use the original name
-      cb(null, file.originalname);
-  }
-});
+const UPLOAD_DIR = './public/data/uploads';
+mkdirp.sync(UPLOAD_DIR);
 
-const upload = multer({ dest: './public/data/uploads/' });
+const storeUpload = async (upload) => {
+    const { createReadStream, filename, mimetype } = await upload.originFileObj.promise;
+    const stream = createReadStream();
+    const id = shortid.generate();
+    const path = `${UPLOAD_DIR}/${id}-${filename}`;
+    const files = { id, filename, mimetype, path };
 
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-var path = require('path');
-var sanitize = require("sanitize-filename");
+    // Store the file in the filesystem.
+    await new Promise((resolve, reject) => {
+        // Create a stream to which the upload will be written.
+        const writeStream = createWriteStream(path);
+
+        // When the upload is fully written, resolve the promise.
+        writeStream.on('finish', resolve);
+
+        // If there's an error writing the file, remove the partially written file
+        // and reject the promise.
+        writeStream.on('error', (error) => {
+            console.log('ERRORRR');
+            unlink(path, () => {
+                reject(error);
+            });
+        });
+
+        // In Node.js <= v13, errors are not automatically propagated between piped
+        // streams. If there is an error receiving the upload, destroy the write
+        // stream with the corresponding error.
+        stream.on('error', (error) => {
+            console.log('ERROR');
+            writeStream.destroy(error);
+        });
+
+        // Pipe the upload into the write stream.
+        stream.pipe(writeStream);
+    });
+
+    // Record the file metadata in the DB.
+    console.log('file');
+    console.log(files);
+    // db.get('uploads').push(file).write()
+
+    return files;
+};
 
 export const Mutation = {
     createTranslationAssignment: rs(TranslationAssignment, {
         before: async (findOptions, { data }, context) => {
-          let err, assignment, fileNames, files;
-    
+          let err, assignment, uploads, uploadRecordsData, uploadRecords;
+          
+          uploads = await Promise.allSettled(
+              data.initialFiles.map(storeUpload),
+          );
+          if (!uploads) {
+            throw err;
+          }
+
           [err, assignment] = await to(TranslationAssignment.create(data));
-          if (err) { throw err;}
+          if (err) {
+            throw err;
+          }
 
-          context.upload();
-
-          // Things I've tried
-          const upload2 = multer({storage: storage}).single(data.initialFiles[0]);
-          
-          const upload_dir= 'public/data/uploads';
-          
-          const fd = new FormData();
-          fd.append('initialFiles', data.initialFiles[0]);
-          const fstream = fs.createWriteStream(upload_dir + data.initialFiles[0].name);
-          data.initialFiles[0].pipe(fstream);
-            fstream.on('close', function () {    
-                console.log("Upload Finished of " + data.initialFiles[0].name);
-            });
-          /*
-          Promise.resolve(data.initialFiles)
-            .each(function(file_incoming, idx) {
-                console.log("  Writing POSTed data :", file_incoming.name);
-                var sanitized_filename = sanitize(file_incoming.name);
-                var file_to_save = path.join(upload_dir, sanitized_filename);
-                
-                return fs
-                  .writeFileAsync(file_to_save, file_incoming.buffer);    
-            })
-            // .catch() code not included for clarity : Clearly you'll need to do some error checking...
-            .then( _ => {
-              console.log("Added files : Success");
-            });
-            */
-
-          fileNames = data.initialFilesNames.map((file) => {
-            return {
-              fileName: `${assignment.id}__${file}`,
-              translationAssignmentId: assignment.id,
-              documentType: 0
-            };
-          });
-          [err, files] = await to(TranslationAssignmentFile.bulkCreate(fileNames));
-          if (err) { throw err;}
-
-          // console.log(data.initialFiles);
+          uploadRecordsData = uploads.map((file) => ({
+            fileName: `${file._settledValueField.id}-${file._settledValueField.filename}`,
+            translationAssignmentId: assignment.id,
+            documentType: 'INITIAL'
+          }));
+          [err, uploadRecords] = await to(TranslationAssignmentFile.bulkCreate(uploadRecordsData));
+          if (err) {
+            throw err;
+          }
 
           return true;
         },
