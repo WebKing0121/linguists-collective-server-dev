5d703e01202f3aa88108357b24610d67213cae62
webking0121 <bestwebdev010@gmail.com>
2020-09-16 11:56:15 +0100

    added back problematic linguist-educational files

---
+++ src/graphql/linguist-educational-institution/linguist-educational-institution.map.ts
@@ -0,0 +1,7 @@
+import { resolver } from 'graphql-sequelize';
+import { LinguistEducationalInstitution } from '../../models';
+
+export const LinguistEducationalInstitutionMap = {
+    linguist: resolver(LinguistEducationalInstitution.associations.linguist),
+    subjectsStudied: resolver(LinguistEducationalInstitution.associations.linguistEducationalInstitutionSubjects)
+};
